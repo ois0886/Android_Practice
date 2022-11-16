f19e7a1a97c2213cf4e40878e12735d771e1cb56
oh in seong <superohinsung@naver.com>
2022-11-16 21:04:04 +0900

    Update MainActivity.kt

--- Practice/app/src/main/java/com/example/mysampleapp/MainActivity.kt
+++ Practice/app/src/main/java/com/example/mysampleapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mysampleapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -13,13 +14,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        auth = Firebase.auth
+        Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
         val joinBtnClicked = findViewById<Button>(R.id.joinBtn)
+        val logoutBtnClicked = findViewById<Button>(R.id.logoutBtn)
+        val logInBtnClicked = findViewById<Button>(R.id.logInBtn)
         joinBtnClicked.setOnClickListener {
-            auth.createUserWithEmailAndPassword("abc@abc.com", "123123")
+            val email = findViewById<EditText>(R.id.emailArea).toString()
+            val password = findViewById<EditText>(R.id.passwordArea).toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
@@ -27,6 +36,15 @@ class MainActivity : AppCompatActivity() {
                         Toast.makeText(this, "no", Toast.LENGTH_SHORT).show()
                     }
                 }
+
+        }
+
+        logoutBtnClicked.setOnClickListener {
+            auth.signOut()
+            Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        logInBtnClicked.setOnClickListener {
 
         }
     }
